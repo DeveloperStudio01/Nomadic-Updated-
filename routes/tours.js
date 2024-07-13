52e48dc1826422f2b528474591a7cab1af778bce
DeveloperStudio01 <studiodevelopers767@gmail.com>
2024-07-13 17:05:07 +0530

    Add files via upload

--- routes/tours.js
+++ routes/tours.js
@@ -5,8 +5,8 @@ import { verifyAdmin } from "../utils/verifyToken.js"
 const router=express.Router()
 
 router.post("/",createTour)//Create New tour
-router.put("/:id",verifyAdmin,updateTour)//Update tour
-router.delete('/:id',verifyAdmin,deleteTour)//Delete Tour
+router.put("/:id",updateTour)//Update tour
+router.delete('/:id',deleteTour)//Delete Tour
 router.get("/:id",getSingleTour)//get 1 tour
 router.get("/search/getRelatedTour",getRelatedTour)//get packages
 router.get('/',getAllTour)//get All tours
--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,6 @@ const router=express.Router()
 router.put("/:id",verifyUser,updateUser)//Update user
 router.delete('/:id',verifyUser,deleteUser)//Delete user
 router.get("/:id",verifyUser,getSingleUser)//get 1 user details
-router.get('/',verifyAdmin,getAllUser)//get All users
+router.get('/',getAllUser)//get All users
 
 export default router
