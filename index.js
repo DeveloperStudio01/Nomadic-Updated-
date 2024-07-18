d48628556b6f8a023ea6c22667f2a1bb4d64cd83
DeveloperStudio01 <studiodevelopers767@gmail.com>
2024-07-19 01:11:44 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const app=express()
 const port=process.env.port || 8000
 
 mongoose.set('strictQuery',false)
-const connect=async()=>{
+const connect=()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URI,{
+         mongoose.connect(process.env.MONGO_URI,{
             useNewUrlParser:true,
             useUnifiedTopology:true,
         });
