0a4dae74b37671e3600a0ffb318afaee8a0fb2b8
DeveloperStudio01 <studiodevelopers767@gmail.com>
2024-07-19 12:20:32 +0530

    tours.js

--- routes/tours.js
+++ routes/tours.js
@@ -1,14 +1,1 @@
-import express from "express"
-import { createTour, deleteTour, getAllTour, getRelatedTour, getSingleTour, updateTour } from "./../controllers/tourController.js"
-import { verifyAdmin } from "../utils/verifyToken.js"
 
-const router=express.Router()
-
-router.post("/",createTour)//Create New tour
-router.put("/:id",updateTour)//Update tour
-router.delete('/:id',deleteTour)//Delete Tour
-router.get("/:id",getSingleTour)//get 1 tour
-router.get("/search/getRelatedTour",getRelatedTour)//get packages
-router.get('/',getAllTour)//get All tours
-
-export default router;
